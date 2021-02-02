16701745a6db7c7f13fad80d5baecdf1827a406e
Jose Mendoza <josanmendozacs@gmail.com>
2021-02-02 07:57:13 -0600

    clase4

---
+++ clase4.js
@@ -0,0 +1,9 @@
+var nombre = "Jose", edad = 33;
+
+function imprimirEdad(n, e) {
+    console.log(`${n} tiene ${e} años`)
+}
+
+imprimirEdad(nombre, edad);
+imprimirEdad('Vicky', 28);
+imprimirEdad('Dario', 24);
