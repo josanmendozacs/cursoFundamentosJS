a5542cac3ffaff960babf8cc640a29c96d53230c
Jose Mendoza <josanmendozacs@gmail.com>
2021-02-01 22:40:45 -0600

    clase2

---
+++ clase2.js
@@ -0,0 +1,13 @@
+var nombre = 'Jose', apellido = 'Mendoza';
+
+var nombreEnMayusculas = nombre.toUpperCase();
+var apellidoEnMinusculas = apellido.toLowerCase();
+
+var primeraLetraDelNombre = nombre.charAt(0);
+var cantidadDeLetrasDelNombre = nombre.length;
+
+var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`;
+
+var str = nombre.substr(1, 2);
+
+var ultimaLetraDelNombre = nombre.charAt(nombre.length -1);
