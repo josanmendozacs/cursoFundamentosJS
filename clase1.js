c9e0e84e272216284e8c347dd0dfaf2313f0dc4b
Jose Mendoza <josanmendozacs@gmail.com>
2021-02-01 22:27:20 -0600

    clase1

---
+++ clase1.js
@@ -0,0 +1,11 @@
+console.log('Hola mundo JS!');
+var nombre;
+
+nombre = 'Freya';
+console.log('Hola ' + nombre);
+
+var nombre = 'José';
+var apellido = 'Mendoza';
+var edad = 33;
+console.log('Hola ' + nombre + ' ' + apellido)
+console.log('Tengo ' + edad + ' años')
