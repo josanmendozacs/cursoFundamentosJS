8e56560b05b83bdef1fbc985c7d41b126c0b8b92
Jose Mendoza <josanmendozacs@gmail.com>
2021-02-02 23:04:59 -0600

    reto2

--- reto2/reto2.js
+++ reto2/reto2.js
@@ -1,3 +1,16 @@
-function esMayorDeEdad() {
-    
+var jose = {
+    nombre: "Jose",
+    edad: 33
+}
+
+var freya = {
+    nombre: "Freya",
+    edad: 17
+}
+
+function esMayorDeEdad(persona) {
+    if (persona.edad >= 18) {
+        console.log(`${persona.nombre} es mayor de edad.`);
+    } else 
+    console.log(`${persona.nombre} no es mayor de edad.`);
 }
