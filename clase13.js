ddda6a632bb92333543a3188f561d302e83cff11
Jose Mendoza <josanmendozacs@gmail.com>
2021-02-03 09:32:50 -0600

    clase 13 final

--- clase13.js
+++ clase13.js
@@ -6,3 +6,21 @@ var jose = {
 }
 
 console.log(`Al inicio del año ${jose.nombre} pesa ${jose.peso} kgs.`);
+
+const INCREMENTO_PESO = 0.2;
+const DIAS_DEL_ANO = 365;
+
+const aumentarDePeso = persona => persona.peso += INCREMENTO_PESO;
+const adelgazar = persona => persona.peso -= INCREMENTO_PESO;
+
+for (var i = 1; i <= DIAS_DEL_ANO; i++) {
+    var random = Math.random()
+
+    if (random < 0.25) {
+        aumentarDePeso(jose)
+    } else if ( random < 0.50) {
+        adelgazar(jose);
+    }
+}
+
+console.log(`Al final del año ${jose.nombre} pesa ${jose.peso.toFixed(1)} kgs.`);
